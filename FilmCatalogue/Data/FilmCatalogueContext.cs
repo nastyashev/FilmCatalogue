f2f7759debcdd4b1749b729bc461c3dd58f806c2
nastyashev <nastya.sova2004@gmail.com>
2024-04-07 02:50:08 +0300

    Add FilmCategory

--- FilmCatalogue/Data/FilmCatalogueContext.cs
+++ FilmCatalogue/Data/FilmCatalogueContext.cs
@@ -9,18 +9,12 @@ namespace FilmCatalogue.Data
 {
     public class FilmCatalogueContext : DbContext
     {
-        // You can add custom code to this file. Changes will not be overwritten.
-        // 
-        // If you want Entity Framework to drop and regenerate your database
-        // automatically whenever you change your model schema, please use data migrations.
-        // For more information refer to the documentation:
-        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
-
         public FilmCatalogueContext() : base("name=FilmCatalogueContext")
         {
         }
 
         public DbSet<Film> Films { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<FilmCategory> FilmCategories { get; set; }
     }
 }
---
+++ FilmCatalogue/Models/FilmCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FilmCatalogue.Models
+{
+    [Table("film_categories")]
+    public class FilmCategory
+    {
+        [Column("Id")]
+        [Key]
+        public int Id { get; set; }
+
+        [Column("film_id")]
+        public int FilmId { get; set; }
+
+        [Column("category_id")]
+        public int CategoryId { get; set; }
+    }
+}
