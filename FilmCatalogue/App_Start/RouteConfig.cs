b1a7d5d030ae1c5123f861fc62788b2769febb3e
nastyashev <nastya.sova2004@gmail.com>
2024-04-09 19:23:05 +0300

    fix layout

--- FilmCatalogue/App_Start/RouteConfig.cs
+++ FilmCatalogue/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace FilmCatalogue
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Film", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
